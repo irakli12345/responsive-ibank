53e1b1d84cb9b3cd0175b379a8167c57ec942fbf
Irakli <irakli@tchigl.com>
2021-08-21 20:53:50 +0400

    starting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import _ from "lodash";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
